3cabc2be3b1da84fc4726cbee0d66933509bb739
Jason Parker <jparker@sangoma.com>
2018-08-21 16:08:47 +0000

    Add missing variable in restore.

--- Restore.php
+++ Restore.php
@@ -3,6 +3,7 @@ namespace FreePBX\modules\Directory;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
   public function runRestore($jobid){
+    $cb = $this->FreePBX->Directory();
     $configs = reset($this->getConfigs());
     foreach($configs['directories'] as $dir){
         $cb->updateDirectory($dir);
